544fc62db798d4d36694fd43545a48bc8fd035d6
victoriahu <vh12490@gmail.com>
2018-11-26 21:18:54 -0800

    "add progress to app tests"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,22 @@ import VideoList from "./VideoList.js"
 import exampleVideoData from "../data/exampleVideoData.js"
 import VideoPlayer from "./VideoPlayer.js"
 
-var App = (props) => (
-  <div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false
+    };
+  }
+  
+  onTitleClick() {
+    var chosenVideo = this;
+  }
+
+  render() {
+    console.log(chosenVideo)
+    return (
+    <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
         <div><h5><em>search</em> view goes here</h5></div>
@@ -17,7 +31,12 @@ var App = (props) => (
         <VideoList videos={exampleVideoData}/>
       </div>
     </div>
-  </div>
+  </div>);
+
+  }
+
+}
+  
 );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
