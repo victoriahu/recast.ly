c5f910c8ab7fc89de5284fa36d53404671c681fe
victoriahu <vh12490@gmail.com>
2018-11-26 20:58:40 -0800

    "add videoplayer"

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,37 +1,15 @@
 var VideoPlayer = (props) => (
-  console.log("VIDEOPLAYER", props);
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src=React.createElement(
-
-  "div",
-  { className: "video-player" },
-  React.createElement(
-    "div",
-    { className: "embed-responsive embed-responsive-16by9" },
-    React.createElement("iframe", { className: "embed-responsive-item", src: {props.video.snippet.thumbnails.default.url}, allowFullScreen: true })
-  ),
-  React.createElement(
-    "div",
-    { className: "video-player-details" },
-    React.createElement(
-      "h3",
-      null,
-      "Video Title"
-    ),
-    React.createElement(
-      "div",
-      null,
-      "Video Description"
-    )
-  ) allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={"https://www.youtube.com/embed/" + props.video.id.videoId} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>{props.video.snippet.title}</h3>
+      <div>{props.video.snippet.description}</div>
     </div>
   </div>
 );
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
